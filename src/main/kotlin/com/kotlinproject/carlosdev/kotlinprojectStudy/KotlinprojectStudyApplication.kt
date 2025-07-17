d2e68fc6d2113070dca17f21cae765ecf08d9ff5
Carlos Alves <carlosaalves07@outlook.com>
2025-07-17 14:13:41 -0300

    feat: initial commit

---
+++ src/main/kotlin/com/kotlinproject/carlosdev/kotlinprojectStudy/KotlinprojectStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinproject.carlosdev.kotlinprojectStudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinprojectStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinprojectStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlinproject/carlosdev/kotlinprojectStudy/KotlinprojectStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinproject.carlosdev.kotlinprojectStudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinprojectStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
